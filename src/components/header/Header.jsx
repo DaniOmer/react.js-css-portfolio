e65d5b1dde186da17f1106adb85cb5da44eb5038
Omer Dotchamou <ddaniomer@gmail.com>
2023-01-15 02:21:24 +0100

    Header component set up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './header.css'
+import CallToAction from './CallToAction'
+import HeaderSocials from './HeaderSocials'
+import Me from '../../assets/Omer.JPG'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <div className="container header_container">
+                <h5>Hello, I'm</h5>
+                <h1>Omer DOTCHAMOU</h1>
+                <h5 className="text-light">Fillstack Developper</h5>
+                <CallToAction />
+                <HeaderSocials />
+
+                <div className="me">
+                    <img src={Me} alt="me" />
+                </div>
+
+                <div>
+                    <a href="contact" className="scroll-down"> Scroll Down</a>
+                </div>
+            </div>
+        </header>
     )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {AiFillGithub} from 'react-icons/ai'
+import {AiFillTwitterCircle} from 'react-icons/ai'
+
+const HeaderSocials = () => {
+    return (
+        <div className="header_socials">
+            <a href="https://www.linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+            <a href="https://www.github.com" target="_blank" rel="noreferrer"><AiFillGithub /></a>
+            <a href="https://www.twitter.com" target="_blank" rel="noreferrer"><AiFillTwitterCircle /></a>
+        </div>
+    )
+}
+
+export default HeaderSocials
